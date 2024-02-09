692c3acbf199d3fcf0a2c5ec221cbea8a97e7905
Petya Todorova <petiatodorova95@gmail.com>
2024-02-09 10:29:36 +0200

    tests

--- src/main/java/com/pages/page_robocorp.java
+++ src/main/java/com/pages/page_robocorp.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
-import java.util.List;
+//import java.util.List;
 
 
 public class page_robocorp {
